f720216b18b0c4fded551af1f3136ac771eb7edd
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-20 12:11:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,7 +2,7 @@
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
-  binding.pry
+
 end
 
 
@@ -11,7 +11,7 @@ def state_log(star_date)
 end
 
 def engage
-  puts state_log(date)
   date = generate_star_date
-  binding.pry
+  puts state_log(date)
+
 end
