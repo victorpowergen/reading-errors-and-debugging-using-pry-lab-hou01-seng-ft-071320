3c73dba3a6ff6360cd6ab6a56cf7aacf91b3587c
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-20 11:51:45 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,7 @@
 # don't forget to add: require 'pry'
 
-def snake_it_up(string.to_i)
-  if string[0] == "s"
+def snake_it_up(string)
+  if string[0] == ["s"]
     10 * ("s").to_i + string.to_i
   else
     string
