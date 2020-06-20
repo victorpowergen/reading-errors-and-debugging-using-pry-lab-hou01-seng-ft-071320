3e6d9b5ca83c60f0012936c2d0856d845a537dbd
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-20 12:28:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,10 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{""} & #{""}"
+  "Hello #{player1} & #{player2}"
 
 end
+start_game
 
 def play_game(player1, player2)
   "#{player1} is better than #{player2}"
